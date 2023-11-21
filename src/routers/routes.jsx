0c2a462196a77911f288ace7be715b336608bead
ositodobbie10 <caamledzeppelin@gmail.com>
2023-11-21 09:55:38 -0600

    'Cambios de nuevo'

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
 import {SingUp,Home,Login,UseContext1,ErrorsingUp,Categorias,Layout2,Configuracion,Perfil, Mapa} from "../index"
 export const MyRoutes = ()=>{
-    //const {data} = props;
-    //const {token} = UseContext1();
+    const {data} = props;
+    const {token} = UseContext1();
     const MyRuta = createBrowserRouter([
         {
             path: "/",
@@ -19,7 +19,43 @@ export const MyRoutes = ()=>{
         },
         {
             path:'/Home',
-            element:<Mapa/>
+           element:<Layout2/>,
+           children:[
+            {
+                index:true,
+                element: token ? <Home/> : <div>No encontrado</div>
+            }
+           ]
+        },
+        {
+            path:"/categorias",
+            element:<Layout2/>,
+            children:[
+                {
+                    index:true,
+                    element:<Categorias/>
+                }
+            ]
+        },
+        {
+            path:"/configurar",
+            element: <Layout2/>,
+            children:[
+                {
+                    index:true,
+                    element: <Configuracion data={data}/>
+                }
+            ]
+        },
+        {
+            path:"/miperfil",
+            element: <Layout2/>,
+            children:[
+                {
+                    index:true,
+                    element: <Perfil/>
+                }
+            ]
         }
        
     ])
