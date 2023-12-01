feb6dadd66990354345ec0003827e973cbc3b8f1
ositodobbie10 <caamledzeppelin@gmail.com>
2023-12-01 09:08:26 -0600

    'Crud categorias'

---
+++ src/superbase/CrudCategorias.jsx
@@ -0,0 +1,48 @@
+import {supabase2} from "../index";
+import Swal from "sweetalert2";
+export const InsertarCategorias = async(p)=>{
+   try {
+    const {data,error} = await supabase2.from("categorias").insert(p).select();
+    if(error){
+        Swal.fire({
+          icon: "error",
+          title: "Fallo...",
+          text:"Ya existe un registro con la base de datos" + p.categorias,
+          timer: 1500,
+          footer:`<a href="">Agrega una nueva descripcion..<a/>`
+        });
+    }
+    if(data){
+        Swal.fire({
+            position:"top-end",
+            icon: "success",
+            title: "Datos Guardados",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+      }
+   } catch (error) {
+    alert(error.error_description || error.message + "insertar categorias")
+   }
+
+}
+
+export const MostrarCategorias = async(idusario)=>{
+    try {
+        const{data} = await supabase2.from("categorias").select().eq("idusuario",idusario).eq("tipo",p.tipo).order("id",{ascending:true});
+        return data;
+    } catch (error) {
+        alert("Error al mostrar las categorias");
+    }
+}
+
+export const BorrarCategorias = async(p)=>{
+    try {
+        const {error} = await supabase2.from("categorias").delete().eq("idusuario",p.idusuario).eq("id",p.id);
+        if(error){
+            alert("error al eliminar",error);
+        }
+    } catch (error) {
+        alert(error.error_description || error.message + "eliminar categorias")   
+    }
+}
